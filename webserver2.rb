c20ce866571e81ad589eeb263dac277886200b77
Jamie-McCaw <psiral@yahoo.com>
2013-01-10 11:05:17 -0500

    Added a first server and cob_spec

---
+++ webserver2.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/ruby
+require "socket"
+require "pry"
+
+port = ARGV.shift
+dir = ARGV.shift
+ 
+def get_content_type(path)
+    ext = File.extname(path)
+    return "text/html"  if ext == ".html" or ext == ".htm"
+    return "image/png"  if ext == ".png"
+    return "text/plain" if ext == ".txt"
+    return "text/css"   if ext == ".css"
+    return "image/jpeg" if ext == ".jpeg" or ext == ".jpg"
+    return "image/gif"  if ext == ".gif"
+    return "image/bmp"  if ext == ".bmp"
+    return "text/plain" if ext == ".rb"
+    return "text/xml"   if ext == ".xml"
+    return "text/xml"   if ext == ".xsl"
+    return "text/html"
+end
+ 
+webserver = TCPServer.new port
+base_dir = Dir.new(".")
+while (session = webserver.accept)
+  request = session.gets
+  trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '').chomp
+  resource =  trimmedrequest
+  if dir.nil?
+    session.puts 'Hello, World!'
+    session.close
+    dir = '.'
+    next
+  end
+  if resource == 'favicon.ico'
+    next
+  end
+  if resource == ""
+    resource = "."
+  end
+ 
+  if !File.exists?(resource)
+    session.print "HTTP/1.1 404/Object Not Found\r\nServer Matteo\r\n\r\n"
+    session.print "404 - Resource cannot be found."
+    session.close
+    next
+  end
+ 
+  if File.directory?(resource)
+    session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
+    Dir.chdir("#{resource}")
+    session.puts Dir.entries("#{dir}").map { |file| "<a href='#{dir}/#{file}'>#{file}</a>"}.join("<br />")
+  else
+    contentType = get_content_type(resource)
+    session.print "HTTP/1.1 200/OK\r\nContent-type: #{contentType}\r\n\r\n"
+    File.open(resource, "rb") do |f|
+      while (!f.eof?) do
+        buffer = f.read(256)
+        session.write(buffer)
+      end
+    end
+  end
+  session.close
+end

b0ccb56a936a09444880569c6a7ea4eac7db1030
Jamie-McCaw <psiral@yahoo.com>
2013-01-23 15:53:18 -0500

    Multi Threaded added

--- webserver2.rb
+++ webserver2.rb
@@ -1,10 +1,10 @@
-#!/usr/bin/ruby
-require "socket"
-require "pry"
+require 'socket'
+require 'pry'
 
 port = ARGV.shift
-dir = ARGV.shift
- 
+dir = File.expand_path(ARGV.shift)
+work_dir = []
+
 def get_content_type(path)
     ext = File.extname(path)
     return "text/html"  if ext == ".html" or ext == ".htm"
@@ -19,46 +19,60 @@ def get_content_type(path)
     return "text/xml"   if ext == ".xsl"
     return "text/html"
 end
- 
-webserver = TCPServer.new port
-while (session = webserver.accept)
-  request = session.gets
-  continue if request.nil?
-  trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '').chomp
-  resource =  trimmedrequest
-  if dir.nil?
-    session.puts 'Hello, World!'
-    session.close
-    dir = '.'
-    next
-  end
-  if resource == 'favicon.ico'
-    next
-  end
-  if resource == ""
-    resource = "."
-  end
- 
-  if !File.exists?(resource)
-    session.print "HTTP/1.1 404/Object Not Found\r\n\r\n"
-    session.print "404 - Resource cannot be found."
-    session.close
-    next
-  end
- 
-  if File.directory?(resource)
-    session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
-    Dir.chdir("#{resource}")
-    session.puts Dir.entries("#{dir}").map { |file| "<a href='#{dir}/#{file}'>#{file}</a>"}.join("<br />")
-  else
-    contentType = get_content_type(resource)
-    session.print "HTTP/1.1 200/OK\r\nContent-type: #{contentType}\r\n\r\n"
-    File.open(resource, "rb") do |f|
-      while (!f.eof?) do
-        buffer = f.read(256)
-        session.write(buffer)
-      end
+
+
+server = TCPServer.open(port)
+puts "Server is ready at port: #{port}\n" 
+loop {                          
+  Thread.start(server.accept) do |session|
+    request = session.gets
+    trimmed_request = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '').chomp
+    resource = trimmed_request
+    if dir.nil?
+      session.puts 'Hello, World!'
+      session.close
+      next
     end
+    if resource == 'favicon.ico'
+    next
+    end
+    if resource == ""
+       resource = dir
+    end
+  
+    puts "Resource: " + resource
+    if !File.exists?(resource)
+      session.print "HTTP/1.1 404/Object Not Found\r\n\r\n"
+      session.print "404 - Resource cannot be found."
+      session.close
+      next
+    end
+    if File.directory?(resource)
+        #Dir.chdir("#{resource}") 
+        puts "Request: "     + request
+        puts "Resource: "    + resource
+        puts "Current Dir: " + Dir.pwd
+        puts "Base dir: "    + dir
+        puts "====================\n"
+    
+        session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
+        session.puts Dir.entries(resource).map { |file|
+          if File.directory?(file)
+            "<a href='#{File.expand_path(file).gsub(dir,"")}'/>#{file}/</a>" 
+          else
+            "<a href='#{File.expand_path(file).gsub(dir,"")}'>#{file}</a>"
+          end
+          }.join("<br />")
+      else
+        contentType = get_content_type(resource)
+        session.print "HTTP/1.1 200/OK\r\nContent-type: #{contentType}\r\n\r\n"
+        File.open(resource, "rb") do |f|
+          while (!f.eof?) do
+            buffer = f.read(256)
+            session.write(buffer)
+          end
+        end
+      end
+    session.close                
   end
-  session.close
-end
+}
